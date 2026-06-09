22e1d1c57355b125c188c9042e002d511e80db84
exxpion <exp1on@yandex.ru>
2026-06-09 11:17:40 +0300

    v1.0.2 - fix autoupdate

--- src/main.js
+++ src/main.js
@@ -45,16 +45,16 @@ function createWindow() {
     setTimeout(() => startServerPing(), 1000);
   });
   if (!process.argv.includes('--dev')) {
-  autoUpdater.autoDownload = false;
-  autoUpdater.allowDowngrade = false;
-  autoUpdater.checkForUpdates().then(result => {
-    if (result && result.updateInfo) {
-      send('launcher-update-ready', true);
-    }
-  }).catch(err => {
-    console.error('Update check failed:', err.message);
-  });
-}
+    autoUpdater.autoDownload = false;
+    autoUpdater.allowDowngrade = false;
+    autoUpdater.checkForUpdates().then(result => {
+      if (result && result.updateInfo) {
+        send('launcher-update-ready', true);
+      }
+    }).catch(err => {
+      console.error('Update check failed:', err.message);
+    });
+  }
 }
 
 app.whenReady().then(createWindow);
